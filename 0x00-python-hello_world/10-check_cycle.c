18df8e93b1330b6ec08164a3ce47998084f298fa
princess585 <princessdkhombisa1994@gmail.com>
2024-01-04 04:14:24 -0800

    Python3

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function that checks a file linked to a cycle
+ * @list: Lists linked to be checked
+ * Return: 1 if has a cycle, and 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+        listint_t *slow = list;
+        listint_t *fast = list;
+
+        if (!list)
+                return (0);
+
+        while (slow && fast && fast->next)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                        return (1);
+        }
+
+        return (0);
+}
